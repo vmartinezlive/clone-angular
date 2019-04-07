1c81ab277f5016e2b777fa62da36e466df680a49
Student <example@example.com>
2019-04-07 15:08:48 -0700

    add array of new apartments in apartment component

---
+++ src/app/apartment.model.ts
@@ -0,0 +1,9 @@
+export class Apartment.Model {
+  constructor (
+    public name: string,
+    public address: string,
+    public bath: number,
+    public beds: number,
+    public id: number,
+  ) { }
+}
